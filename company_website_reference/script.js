353db5c88424c1f2dcfaf670601fb8672f70005c
us_kyoshi <90842745+kyoxi@users.noreply.github.com>
2023-10-04 22:11:29 +0700

    optimized the code to run faster and readable
    changes some syntax to easier to read

--- company_website_reference/script.js
+++ company_website_reference/script.js
@@ -1,26 +1,23 @@
-let character = 
-document.getElementById("character");
-let block = 
-document.getElementById("block");
-function jump(){
-    character.classList.add("animate");
-    setTimeout(function(){
-        character.classList.remove("animate");
-    },500);
+const character = document.getElementById("character");
+const block = document.getElementById("block");
+
+function jump() {
+  character.classList.add("animate");
+  setTimeout(function () {
+    character.classList.remove("animate");
+  }, 500);
 }
 
-let checkDead = setInterval(function(){
-    let characterTop =
-    parseInt(window.getComputedStyle(character)
-        .getPropertyValue("top"));
+function checkDead() {
+  const characterTop = parseInt(window.getComputedStyle(character).top);
+  const blockLeft = parseInt(window.getComputedStyle(block).left);
 
-let blockLeft =
-    parseInt(window.getComputedStyle(block)
-        .getPropertyValue("left"));
-        if(blockLeft<20 && blockLeft>0 && characterTop>=130){
-            block.style.animation = "none";
-            block.style.display = "none"
-            alert("bruh")
-        }
-},10);
+  if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+    block.style.animation = "none";
+    block.style.display = "none";
+    alert("bruh");
+  }
+}
+
+setInterval(checkDead, 10);
 
